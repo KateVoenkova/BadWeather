f81bbc2108f4de3a7faaf3c52b1805b2caf53645
Ekaterina Voenkova <env906@gmail.com>
2025-05-20 20:01:04 +0300

    Исправленная

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ db = SQLAlchemy()
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
